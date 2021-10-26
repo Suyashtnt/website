0182fc74a412c10df05726073a73dbcf128d105e
Just a Badly Drawn TABS Dude <45307955+Suyashtnt@users.noreply.github.com>
2021-10-26 17:51:02 +0200

    feat: yes

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <nav w:h="12" w:shadow="lg">
+            <h1 w:text="xl">TNT MAN INC</h1>
+        </nav>
+        <slot />
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,135 @@
+<template>
+  <div w:w="screen" w:grid="~ cols-5" w:justify="center">
+    <div></div>
+    <div class="glitch" w:grid="col-span-3">
+      <glitched-writer
+        class="w:w-full text-center"
+        :text="phrase"
+        appear
+        v-for="idx in [1, 2, 3]"
+        :key="idx"
+      ></glitched-writer>
+    </div>
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import GlitchedWriter from 'vue-glitched-writer'
+
+const phrases = [
+  "TNT MAN INC",
+  "COMING SOON"
+]
+
+let idx = 0
+
+let phrase = ref(phrases[0])
+
+setInterval(() => {
+  idx++
+  if (idx >= phrases.length) {
+    idx = 0
+  }
+
+  phrase.value = phrases[idx]
+}, 7000)
+</script>
+
+<style>
+body {
+  background: #222;
+  min-height: 100vh;
+  display: grid;
+  color: white;
+  font-family: sans-serif;
+}
+
+.glitch {
+  font-size: 6.5rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  position: relative;
+
+  text-shadow: 0.05em 0 0 rgba(255, 0, 0, 0.75),
+    -0.025em -0.05em 0 rgba(0, 255, 0, 0.75),
+    0.025em 0.05em 0 rgba(0, 0, 255, 0.75);
+
+  animation: glitch 500ms infinite;
+}
+
+.glitch span {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.glitch span:first-child {
+  animation: glitch 650ms infinite;
+  clip-path: polygon(0 0, 100% 0, 100% 45%, 0 45%);
+  transform: translate(-0.025em, -0.0125em);
+  /* color: green; */
+  opacity: 0.8;
+}
+
+.glitch span:last-child {
+  animation: glitch 375ms infinite;
+  clip-path: polygon(0 80%, 100% 20%, 100% 100%, 0 100%);
+  transform: translate(0.0125em, 0.025em);
+  /* color: red; */
+  opacity: 0.8;
+}
+
+/* https://web.dev/prefers-reduced-motion/#(bonus)-forcing-reduced-motion-on-all-websites */
+
+@keyframes glitch {
+  0% {
+    text-shadow: 0.05em 0 0 rgba(255, 0, 0, 0.75),
+      -0.05em -0.025em 0 rgba(0, 255, 0, 0.75),
+      -0.025em 0.05em 0 rgba(0, 0, 255, 0.75);
+  }
+  14% {
+    text-shadow: 0.05em 0 0 rgba(255, 0, 0, 0.75),
+      -0.05em -0.025em 0 rgba(0, 255, 0, 0.75),
+      -0.025em 0.05em 0 rgba(0, 0, 255, 0.75);
+  }
+  15% {
+    text-shadow: -0.05em -0.025em 0 rgba(255, 0, 0, 0.75),
+      0.025em 0.025em 0 rgba(0, 255, 0, 0.75),
+      -0.05em -0.05em 0 rgba(0, 0, 255, 0.75);
+  }
+  49% {
+    text-shadow: -0.05em -0.025em 0 rgba(255, 0, 0, 0.75),
+      0.025em 0.025em 0 rgba(0, 255, 0, 0.75),
+      -0.05em -0.05em 0 rgba(0, 0, 255, 0.75);
+  }
+  50% {
+    text-shadow: 0.025em 0.05em 0 rgba(255, 0, 0, 0.75),
+      0.05em 0 0 rgba(0, 255, 0, 0.75), 0 -0.05em 0 rgba(0, 0, 255, 0.75);
+  }
+  99% {
+    text-shadow: 0.025em 0.05em 0 rgba(255, 0, 0, 0.75),
+      0.05em 0 0 rgba(0, 255, 0, 0.75), 0 -0.05em 0 rgba(0, 0, 255, 0.75);
+  }
+  100% {
+    text-shadow: -0.025em 0 0 rgba(255, 0, 0, 0.75),
+      -0.025em -0.025em 0 rgba(0, 255, 0, 0.75),
+      -0.025em -0.05em 0 rgba(0, 0, 255, 0.75);
+  }
+}
+
+@media (prefers-reduced-motion: reduce) {
+  *,
+  ::before,
+  ::after {
+    animation-delay: -1ms !important;
+    animation-duration: 1ms !important;
+    animation-iteration-count: 1 !important;
+    background-attachment: initial !important;
+    scroll-behavior: auto !important;
+    transition-duration: 0s !important;
+    transition-delay: 0s !important;
+  }
+}
+</style>
