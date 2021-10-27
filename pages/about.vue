bf54e6f50c5ffab7b50995bbbb3f92dbce0a3d7b
Just a Badly Drawn TABS Dude <45307955+Suyashtnt@users.noreply.github.com>
2021-10-27 20:02:34 +0200

    feat: :sparkles: about page

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="text-center">
+        <p class="text-9xl">Stuff should be here, but its</p>
+        <glitched-writer text="COMING SOON" class="glitch" appear />
+    </div>
+</template>
+
+<script setup>
+import GlitchedWriter from 'vue-glitched-writer'
+</script>
