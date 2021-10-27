c7feee7cf986ab76f84bdb81593e32d2b1e4d983
Just a Badly Drawn TABS Dude <45307955+Suyashtnt@users.noreply.github.com>
2021-10-27 20:03:36 +0200

    feat: :sparkles: make the homepage look cooler

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="w-screen grid grid-cols-4 justify-center">
-    <div></div>
-    <div class="col-span-3">
-      <glitched-writer class="w:w-full text-center glitch" :text="phrase" appear></glitched-writer>
+  <div>
+    <p class="text-center text-4xl">
+      <glitched-writer class="w-full" :text="smallText" appear></glitched-writer>
+    </p>
+    <div class="text-center">
+      <glitched-writer class="w:w-full text-center glitch" :text="bigText" appear></glitched-writer>
     </div>
-    <div></div>
   </div>
 </template>
 
@@ -12,118 +13,28 @@
 import { ref } from 'vue'
 import GlitchedWriter from 'vue-glitched-writer'
 
-const phrases = [
+const bigTexts = [
   "TNT MAN INC",
   "COMING SOON"
 ]
 
-let idx = 0
+const smallTexts = [
+  "WELCOME TO",
+  "The website of a really bad coder. Its really bare-bones for now, but actual content is"
+]
 
-let phrase = ref(phrases[0])
+let idx = ref(0)
+
+let bigText = ref(bigTexts[0])
+let smallText = ref(smallTexts[0])
 
 setInterval(() => {
-  idx++
-  if (idx >= phrases.length) {
-    idx = 0
+  idx.value++
+  if (idx.value >= bigTexts.length) {
+    idx.value = 0
   }
 
-  phrase.value = phrases[idx]
+  bigText.value = bigTexts[idx.value]
+  smallText.value = smallTexts[idx.value]
 }, 7000)
 </script>
-
-<style>
-body {
-  background: #222;
-  min-height: 100vh;
-  display: grid;
-  color: white;
-  font-family: sans-serif;
-}
-
-.glitch {
-  font-size: 6.5rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  position: relative;
-
-  text-shadow: 0.05em 0 0 rgba(255, 0, 0, 0.75),
-    -0.025em -0.05em 0 rgba(0, 255, 0, 0.75),
-    0.025em 0.05em 0 rgba(0, 0, 255, 0.75);
-
-  animation: glitch 500ms infinite;
-}
-
-.glitch span {
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-
-.glitch span:first-child {
-  animation: glitch 650ms infinite;
-  clip-path: polygon(0 0, 100% 0, 100% 45%, 0 45%);
-  transform: translate(-0.025em, -0.0125em);
-  /* color: green; */
-  opacity: 0.8;
-}
-
-.glitch span:last-child {
-  animation: glitch 375ms infinite;
-  clip-path: polygon(0 80%, 100% 20%, 100% 100%, 0 100%);
-  transform: translate(0.0125em, 0.025em);
-  /* color: red; */
-  opacity: 0.8;
-}
-
-/* https://web.dev/prefers-reduced-motion/#(bonus)-forcing-reduced-motion-on-all-websites */
-
-@keyframes glitch {
-  0% {
-    text-shadow: 0.05em 0 0 rgba(255, 0, 0, 0.75),
-      -0.05em -0.025em 0 rgba(0, 255, 0, 0.75),
-      -0.025em 0.05em 0 rgba(0, 0, 255, 0.75);
-  }
-  14% {
-    text-shadow: 0.05em 0 0 rgba(255, 0, 0, 0.75),
-      -0.05em -0.025em 0 rgba(0, 255, 0, 0.75),
-      -0.025em 0.05em 0 rgba(0, 0, 255, 0.75);
-  }
-  15% {
-    text-shadow: -0.05em -0.025em 0 rgba(255, 0, 0, 0.75),
-      0.025em 0.025em 0 rgba(0, 255, 0, 0.75),
-      -0.05em -0.05em 0 rgba(0, 0, 255, 0.75);
-  }
-  49% {
-    text-shadow: -0.05em -0.025em 0 rgba(255, 0, 0, 0.75),
-      0.025em 0.025em 0 rgba(0, 255, 0, 0.75),
-      -0.05em -0.05em 0 rgba(0, 0, 255, 0.75);
-  }
-  50% {
-    text-shadow: 0.025em 0.05em 0 rgba(255, 0, 0, 0.75),
-      0.05em 0 0 rgba(0, 255, 0, 0.75), 0 -0.05em 0 rgba(0, 0, 255, 0.75);
-  }
-  99% {
-    text-shadow: 0.025em 0.05em 0 rgba(255, 0, 0, 0.75),
-      0.05em 0 0 rgba(0, 255, 0, 0.75), 0 -0.05em 0 rgba(0, 0, 255, 0.75);
-  }
-  100% {
-    text-shadow: -0.025em 0 0 rgba(255, 0, 0, 0.75),
-      -0.025em -0.025em 0 rgba(0, 255, 0, 0.75),
-      -0.025em -0.05em 0 rgba(0, 0, 255, 0.75);
-  }
-}
-
-@media (prefers-reduced-motion: reduce) {
-  *,
-  ::before,
-  ::after {
-    animation-delay: -1ms !important;
-    animation-duration: 1ms !important;
-    animation-iteration-count: 1 !important;
-    background-attachment: initial !important;
-    scroll-behavior: auto !important;
-    transition-duration: 0s !important;
-    transition-delay: 0s !important;
-  }
-}
-</style>
