f06d981a9a6814ceb798bddd3ef104ce9745af23
Just a Badly Drawn TABS Dude <45307955+Suyashtnt@users.noreply.github.com>
2021-10-27 19:00:13 +0200

    feat: :mag: haha meta tags go brrrr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,20 @@ import { defineNuxtConfig } from "nuxt3";
 
 export default defineNuxtConfig({
   buildModules: ["nuxt-windicss"],
+  meta: {
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      {
+        name: "description",
+        content:
+          "Welcome To TNT MAN INC. The place where the most random stuff is mashed together into this monstrocity of a website",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+    ],
+  },
 });
