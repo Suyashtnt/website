5f7af659c982953ba7c723961b3ee8accae226ef
Just a Badly Drawn TABS Dude <45307955+Suyashtnt@users.noreply.github.com>
2021-10-27 20:02:49 +0200

    feat: :sparkles: add button to about page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,21 @@
 <template>
     <Title>{{ $route.name === 'index' ? 'Home' : $route.name }} | TNT MAN INC</Title>
     <div>
-        <nav class="shadow-lg h-24 flex flex-row items-center">
+        <nav role="navigation" class="shadow-lg h-24 flex flex-row items-center mb-12">
             <h1 class="text-4xl ml-4">TNT MAN INC</h1>
             <div class="flex-grow"></div>
             <div class="flex flex-row mr-8">
                 <button class="btn" @click="$router.push('/')">Home</button>
+                <button class="btn" @click="$router.push('/about')">about</button>
             </div>
         </nav>
         <slot />
     </div>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
+import '~/assets/glitch.css'
+import type { } from 'vue-router'
 </script>
 
 <style>
