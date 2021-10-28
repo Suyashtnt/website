897df48fd99051bf2a737b83e9fe261815f2d499
Just a Badly Drawn TABS Dude <45307955+Suyashtnt@users.noreply.github.com>
2021-10-28 15:05:45 +0200

    style: :art: refactor buttons

---
+++ components/btn.vue
@@ -0,0 +1,5 @@
+<template>
+    <button class="ripple-bg-blue-600 shadow-md rounded-md p-6 ml-2 mr-2 h-6 flex items-center">
+        <slot />
+    </button>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,8 +5,8 @@
             <h1 class="text-4xl ml-4">TNT MAN INC</h1>
             <div class="flex-grow"></div>
             <div class="flex flex-row mr-8">
-                <button class="btn" @click="$router.push('/')">Home</button>
-                <button class="btn" @click="$router.push('/about')">About</button>
+                <btn @click="$router.push('/')">Home</btn>
+                <btn @click="$router.push('/about')">About</btn>
             </div>
         </nav>
         <slot />
@@ -14,19 +14,6 @@
 </template>
 
 <script setup lang="ts">
-import '~/assets/glitch.css'
+import '~/assets/glitch.scss'
 import type { } from 'vue-router'
 </script>
-
-<style>
-.btn {
-    @apply ripple-bg-blue-600
-    shadow-md
-    rounded-md
-    p-6 
-    ml-2 mr-2 
-    h-6
-    flex 
-    items-center;
-}
-</style>
