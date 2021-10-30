cf580f1ccec54062ae32fb74409f326b9ab0c545
Just a Badly Drawn TABS Dude <45307955+Suyashtnt@users.noreply.github.com>
2021-10-30 11:55:31 +0200

    feat: :sparkles: add more games

--- components/home/sections/about.vue
+++ components/home/sections/about.vue
@@ -25,7 +25,7 @@
             >
                 <tippy arrow>
                     <template #content>
-                        <div class="bg-gray-900 rounded-lg p-6">
+                        <div class="bg-gray-900 rounded-lg pt-5 pb-5 pl-2">
                             <div class="flex flex-row">
                                 <img
                                     :src="iconUrl"
@@ -34,10 +34,10 @@
                                 />
                                 <div class="text-5xl self-center">{{ name }}</div>
                             </div>
-                            <p class="prose mt-6">{{ description }}</p>
+                            <p class="mt-6" v-html="description" />
                         </div>
                     </template>
-
+                    <!-- workaround for now -->
                     <span>{{ name }}, {{ "" }}</span>
                 </tippy>
             </a>
@@ -55,15 +55,39 @@ interface Game {
     iconUrl: string;
 }
 
-const gameList: Game[] = [{
-    name: 'TABG',
-    description: 'A ragdoll-based FPS made for april fools. Cursed as hell. Fun as hell.',
-    iconUrl: "https://thumb.modcdn.io/mods/d4ff/380792/thumb_1020x2000/logo.png",
-    link: "https://store.steampowered.com/app/823130/Totally_Accurate_Battlegrounds/"
-}, {
-    name: 'DSP',
-    description: 'Dyson Sphere program is a factory game, but its in space over multiple stars and planets. Will test also your mental maths skills.',
-    iconUrl: "https://sun9-79.userapi.com/impg/C9f8lFIj8_S1aeZvmRJICRrmRd1-DiE1aGpwQQ/hGi0W42U1xM.jpg?size=200x200&quality=96&sign=f3ff878566d2af48c036968aaf6c0ab9&type=album",
-    link: "https://store.steampowered.com/app/1366540/Dyson_Sphere_Program/"
-}]
+const gameList: Game[] = [
+    {
+        name: 'TABG',
+        description: 'A ragdoll-based FPS made for april fools. Cursed as hell. Fun as hell.',
+        iconUrl: "https://thumb.modcdn.io/mods/d4ff/380792/thumb_1020x2000/logo.png",
+        link: "https://store.steampowered.com/app/823130/Totally_Accurate_Battlegrounds/"
+    }, {
+        name: 'DSP',
+        description: 'Dyson Sphere program is a factory game, but its in space over multiple stars and planets. Will test also your mental maths skills.',
+        iconUrl: "https://sun9-79.userapi.com/impg/C9f8lFIj8_S1aeZvmRJICRrmRd1-DiE1aGpwQQ/hGi0W42U1xM.jpg?size=200x200&quality=96&sign=f3ff878566d2af48c036968aaf6c0ab9&type=album",
+        link: "https://store.steampowered.com/app/1366540/Dyson_Sphere_Program/"
+    },
+    {
+        name: 'ULTRAKILL',
+        description: `
+MANKIND IS DEAD <br>
+HELL IS FULL <br>
+<span class="text-red-700">BLOOD IS FUEL</span>
+    `,
+        iconUrl: "https://styles.redditmedia.com/t5_2etapp/styles/communityIcon_8vtgmv7fpjf41.png?width=256&s=7d0a2e48fe3b21d28bc1904de9b2cf865ce003e5",
+        link: "https://store.steampowered.com/app/1229490/ULTRAKILL/"
+    },
+    {
+        name: 'Minecraft',
+        description: 'If you dont know minecraft then idk how you found my website',
+        iconUrl: 'https://www.freeiconspng.com/download/16689',
+        link: 'https://www.minecraft.net'
+    },
+    {
+        name: 'Flying Gorilla',
+        description: 'The most blursed mobile game someone has made. And I\'m somehow in the top 2% of players',
+        iconUrl: 'https://play-lh.googleusercontent.com/34GKHRxH_V6ILfiH5Pmom8xflEzAwJdNMYXy4eLEYV0sS9SMH8YvyuIr-zZ8TfZ7GU97=s180-rw',
+        link: 'https://apps.apple.com/us/app/flying-gorilla/id1365028549'
+    }
+]
 </script>
